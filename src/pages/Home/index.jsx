293d72e3643551d8a0c7d4fcb06621b4652e704c
Ricardo Andres Castro Ahumada <ricardocastro.ti@gmail.com>
2020-12-26 00:34:26 -0300

    logo e campo de pesquisa

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
-import { Container } from './style'
+import React, { useState } from 'react';
+import { Container, Img, Search } from './style';
+import TextField, { Input } from '@material/react-text-field';
+import logo from '../../assets/logo.svg';
 
 export default function Home() {
+  const [input, setInput] = useState('');
+
   return (
-    <Container>Oi</Container>
+    <Container>
+      <Search>
+        <Img src={logo} alt="logo" />
+        <TextField label="Pesquisar" outlined>
+          <Input value={input}
+            onChange={(e) => setInput(e.target.value)} />
+        </TextField>
+      </Search>
+    </Container>
   )
 }
--- src/pages/Home/style.js
+++ src/pages/Home/style.js
@@ -1,5 +1,21 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
-  background-color: ${({ theme }) => theme.colors.background}
-`
+export const Container = styled.aside`
+  background-color: ${({ theme }) => theme.colors.background};
+  width: 360px;
+  height: 100vh;
+  overflow-y: auto;
+`;
+
+export const Search = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: #ffffff;
+  padding: 16px;
+`;
+
+export const Img = styled.img`
+  width: 166px;
+`;
